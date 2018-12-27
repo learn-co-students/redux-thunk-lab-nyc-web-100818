d2a0a8ca8855896a547bde311ce15069745a3d01
ihollander <ianholla@gmail.com>
2018-12-27 07:43:58 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { Navbar } from 'react-bootstrap'
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
 
-class App extends Component {   
-  
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchCats()
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,12 +20,18 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        {!this.props.loading && <CatList catPics={this.props.catPics} />}
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return { 
+    loading: state.cats.loading,
+    catPics: state.cats.pictures 
+  }
+}
 
-
-export default App
+export default connect(mapStateToProps, { fetchCats })(App)
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
 import App from './App'
 
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
